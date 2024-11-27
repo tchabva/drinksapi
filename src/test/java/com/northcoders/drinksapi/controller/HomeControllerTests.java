97b95789d4e96dbff6025a6b5be57c38f70b311b
Tawanda Chabva <tchabva@gmail.com>
2024-11-27 16:48:01 +0000

    refactor: added comments to HomeController to provide context

--- src/test/java/com/northcoders/drinksapi/controller/HomeControllerTests.java
+++ src/test/java/com/northcoders/drinksapi/controller/HomeControllerTests.java
@@ -7,18 +7,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+/*
+The @AutoConfigureMockMvc is an annotation that tells the test class to enable & configure auto-configuration of MockMvc.
+MockMvc is the Main entry point for server-side Spring MVC testing its useful because you can test the controller
+without running the controller within a server
+ */
 @AutoConfigureMockMvc
 @SpringBootTest
 public class HomeControllerTests {
+
+    /*
+    This @Autowired is part of Spring it enables objects(beans) to be injected at runtime by Spring Dependency Injection
+    mechanism
+     */
     @Autowired
     private MockMvc mockMvcController;
 
     @Test
     public void testGetHome() throws Exception {
+        //Arrange, Act and Assert chained within the following statements
 
         String expectedContent = "Welcome to the Drinks API!";
 
+        /*
+        Make a GET request to the '/' endpoint
+        Matchers are helpful methods that help you write your assertions
+        According to the requirement, we expect 200 OK Status Code as a Response
+        According to the requirement, we expect the string as a Response
+         */
         this.mockMvcController.perform(
                         MockMvcRequestBuilders.get("/"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
