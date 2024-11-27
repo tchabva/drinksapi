4c510e34d36b03fce72e8d25cb5cb1de510674dc
Tawanda Chabva <tchabva@gmail.com>
2024-11-27 13:43:07 +0000

    refactor: changed the Coffee model to a record

--- src/main/java/com/northcoders/drinksapi/model/Coffee.java
+++ src/main/java/com/northcoders/drinksapi/model/Coffee.java
@@ -1,20 +1,5 @@
 package com.northcoders.drinksapi.model;
 
-public class Coffee {
-    private long id;
-    private String name;
-
-    public Coffee(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
+public record Coffee(long id, String name) {
 
 }
