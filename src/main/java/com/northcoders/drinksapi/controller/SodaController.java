2e86830d3ab3a01ca08a1ca9f910917630519d31
Tawanda Chabva <tchabva@gmail.com>
2024-11-27 14:15:14 +0000

    feat: implemented soda method in the SodaController

--- src/main/java/com/northcoders/drinksapi/controller/SodaController.java
+++ src/main/java/com/northcoders/drinksapi/controller/SodaController.java
@@ -1,13 +1,25 @@
 package com.northcoders.drinksapi.controller;
 
+import com.northcoders.drinksapi.model.Soda;
+import com.northcoders.drinksapi.service.SodaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SodaController {
 
+    @Autowired
+    SodaService sodaService;
+
     @GetMapping("/sodalover")
     public String sodaLover(){
         return "I like Soda!";
+    }
+
+    @GetMapping("/soda")
+    public Soda soda(@RequestParam(value = "name", defaultValue = "coke") String name){
+        return sodaService.newSoda(name);
     }
 }
