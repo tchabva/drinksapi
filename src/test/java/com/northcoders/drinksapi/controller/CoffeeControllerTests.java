bfa9b7816afb58a4f8911ec15a4fd72e9936887a
Kaidenn J <teamplayrite@gmail.com>
2024-11-27 11:10:09 +0000

    test: add test for CoffeeController

---
+++ src/test/java/com/northcoders/drinksapi/controller/CoffeeControllerTests.java
@@ -0,0 +1,27 @@
+package com.northcoders.drinksapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CoffeeControllerTests {
+    @Autowired
+    private MockMvc mockMvcController;
+
+    @Test
+    public void testCoffeeLover() throws Exception {
+
+        String expectedContent = "I like Coffee!";
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/coffeelover"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedContent));
+    }
+}
