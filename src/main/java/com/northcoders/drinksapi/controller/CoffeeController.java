81458a8a000b8ab71e89ba2e5a4224e04d6a1774
Tawanda Chabva <tchabva@gmail.com>
2024-11-27 12:12:24 +0000

    feat: implemented the coffee method in the CoffeeController

--- src/main/java/com/northcoders/drinksapi/controller/CoffeeController.java
+++ src/main/java/com/northcoders/drinksapi/controller/CoffeeController.java
@@ -1,12 +1,25 @@
 package com.northcoders.drinksapi.controller;
 
+import com.northcoders.drinksapi.model.Coffee;
+import com.northcoders.drinksapi.service.CoffeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class CoffeeController {
+
+    @Autowired
+    CoffeeService coffeeService;
 
     @GetMapping("/coffeelover")
     public String coffeeLover(){
         return "I like Coffee!";
+    }
+
+    @GetMapping("/coffee")
+    public Coffee coffee(@RequestParam(value = "name", defaultValue = "latte") String name){
+        return coffeeService.newCoffee(name);
     }
 }
